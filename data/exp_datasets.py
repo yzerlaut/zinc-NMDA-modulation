515589c026342c5100133c5f38567a9e5d8e5cf4
Yann Zerlaut <yann.zerlaut@gmail.com>
2020-07-06 15:12:07 +0200

    adding calib folder

--- data/exp_datasets.py
+++ data/exp_datasets.py
@@ -1,14 +1,14 @@
 VC_STEPS_DATASET = {'20Hz_protocol':[\
-                                     {'Control':['nm02Mar2018c0_000_ctrl.h5'],
-                                      'Tricine':['nm02Mar2018c0_001_tricine.h5']},
-                                     {'Control':['nm02Mar2018c1_000_ctrl.h5'],
-                                      'Tricine':['nm02Mar2018c1_002_tricine.h5']},
-                                     {'Control':['nm02Mar2018c2_000_ctrl.h5'],
-                                      'Tricine':['nm02Mar2018c2_002_tricine.h5']},
-                                     {'Control':['nm30Mar2018c1_000_ctrl.h5'],
-                                      'Tricine':['nm30Mar2018c1_004_tricine.h5']},
-                                     {'Control':['nm17Apr2019c1_000.h5'],
-                                     'Tricine':['nm17Apr2019c1_001.h5']},
+                                     {'Control':['nm02Mar2018c0_000.h5'],
+                                      'Tricine':['nm02Mar2018c0_001.h5']},
+                                     {'Control':['nm02Mar2018c1_000.h5'],
+                                      'Tricine':['nm02Mar2018c1_002.h5']},
+                                     {'Control':['nm02Mar2018c2_000.h5'],
+                                      'Tricine':['nm02Mar2018c2_002.h5']},
+                                     {'Control':['nm30Mar2018c1_000.h5'],
+                                      'Tricine':['nm30Mar2018c1_004.h5']},
+                                     # {'Control':['nm17Apr2019c1_000.h5'],
+                                     # 'Tricine':['nm17Apr2019c1_001.h5']},
                             ],
                     '3Hz_protocol':[\
                                     {'Control':['nm18Sep2019c1_000.h5'],
@@ -17,8 +17,8 @@ VC_STEPS_DATASET = {'20Hz_protocol':[\
                                      'Tricine':['nm19Sep2019c1_002.h5']},
                                     {'Control':['nm20Sep2019c1_001.h5'],
                                      'Tricine':['nm20Sep2019c1_003.h5']},
-                                    {'Control':['nm30Aug2019c0_000.h5'],
-                                    'Tricine':['nm30Aug2019c0_002.h5']},
+                                    # {'Control':['nm30Aug2019c0_000.h5'],
+                                    # 'Tricine':['nm30Aug2019c0_002.h5']},
                                     {'Control':['nm18Sep2019c4_000.h5'],
                                      'Tricine':['nm18Sep2019c4_003.h5']},
                                     {'Control':['nm19Sep2019c2_001.h5'],
@@ -27,6 +27,20 @@ VC_STEPS_DATASET = {'20Hz_protocol':[\
                                      'Tricine':['nm23Sep2019c2_002.h5']},
                     ],
 }
+
+
+'nm19May22c1' # 000, 001, 002
+'nm20May2019c6' # 000, 001, 002
+'nm20May2019c5' # 000, 001, 002, 003
+'nm19May2019c7' # 000, 001, 002
+'nm13May2019c5' # 000, 001, 002, 003
+'nm27May2019c7' # 000, 001, 002, 003
+'nm27May2019c8' # 000, 001, 002, 003
+
+								
+190501c4	190502c0	190502c2	190502c4	190502c5	190503c3c1	190503c3	190503c7	200115c0
+								
+190411c2	190418c2	180419c5	180419c9	190425c5	190506c1	190507c1	190508c5	
 
 
 # recorded at -80mV (AMPA), and +20mV (NMDA) after correction for liquid junction potential
