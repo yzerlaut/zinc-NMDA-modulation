32065d5c1a11af2857f8f947fbdad96dd98542c9
yzerlaut <yann.zerlaut@gmail.com>
2020-08-19 12:32:40 +0200

    save working grid as -0

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ Model = {
     'tstop':600, # [ms]
     'dt':0.1,# [ms]
     'seed':1, #
-    'VC-gL-reduction-factor':10, #
+    'VC-gL-reduction-factor':5, #
     'VC-discard-time':200, # [ms]
     'VC-gclamp':1,# [uS]
     'VC-cmd':20,# [mV]
@@ -72,15 +72,15 @@ Model = {
     'gL_min':0.02, 'gL_max':2., 'N_gL':30,
     'cm_min':0.5, 'cm_max':2., 'N_cm':30,
     'tauDecayNMDA_min':60, 'tauDecayNMDA_max':120, 'N_tauDecayNMDA':7,
-    'Nsyn1_min':1, 'Nsyn1_max':12, 'N_Nsyn1':6,
-    'Nsyn2_min':1, 'Nsyn2_max':12, 'N_Nsyn2':6,
+    'Nsyn1_min':2, 'Nsyn1_max':24, 'N_Nsyn1':6,
+    'Nsyn2_min':2, 'Nsyn2_max':24, 'N_Nsyn2':6,
     'Tnsyn20Hz_min':30, 'Tnsyn20Hz_max':70, 'N_Tnsyn20Hz':4,
-    'Tnsyn3Hz_min':30, 'Tnsyn3Hz_max':70, 'N_Tnsyn3Hz':4,
+    'Tnsyn3Hz_min':70, 'Tnsyn3Hz_max':1500, 'N_Tnsyn3Hz':4,
     'alphaZn_min':0.15, 'alphaZn_max':0.5, 'N_alphaZn':7,
-    'tauRiseZn_min':5, 'tauRiseZn_max':50, 'N_tauRiseZn':6,
-    'tauDecayZn_min':50, 'tauDecayZn_max':500, 'N_tauDecayZn':6,
+    'tauRiseZn_min':5, 'tauRiseZn_max':59, 'N_tauRiseZn':7,
+    'tauDecayZn_min':50, 'tauDecayZn_max':590, 'N_tauDecayZn':7,
     'Deltax0_min':0.3, 'Deltax0_max':0.7, 'N_Deltax0':5,
-    'deltax_min':0.05, 'deltax_max':0.3, 'N_deltax':5,
+    'deltax_min':0.05, 'deltax_max':0.15, 'N_deltax':3,
     #############################################################
     # ---------- SYNAPTIC STIMULATION PARAMS  ----------------- #
     #############################################################
