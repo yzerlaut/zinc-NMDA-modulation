f793fd843de0abdeacf9bf3a87dcaf469b9f0904
yzerlaut <yann.zerlaut@gmail.com>
2020-08-26 11:57:10 +0200

    starting to re-update free-Zinc fitting

--- calibration_runs.py
+++ calibration_runs.py
@@ -145,8 +145,9 @@ def compute_free_residual(sim, index, calib_data, condition='free'):
             first_peak_cond = (calib_data['t_%s' % cond]<calib_data['DT0_%s' % cond]+30)
 
             factor_exp = np.max(trace_exp[first_peak_cond])
-            # trace_exp /= factor_exp
             factor_exp0 = np.max(trace_exp0[first_peak_cond])
+            trace_exp *= factor_exp0/factor_exp
+            # trace_exp /= factor_exp
             # trace_exp0 /= factor_exp0
 
             # normalizing to peak in the model
@@ -160,9 +161,9 @@ def compute_free_residual(sim, index, calib_data, condition='free'):
 
             # Residual *= 1./np.corrcoef(trace_model,trace_exp)[0][1]
             # print(Residual, np.corrcoef(trace_model,trace_exp)[0][1])
-            Residual *= 1+np.sum((trace_model-trace_exp)**2)/np.sum(trace_exp**2)
-            Residual *= 1+np.abs(1./np.corrcoef(diff_model,diff_exp)[0][1])
-            # Residual *= 1+np.sum((diff_model-diff_exp)**2)/np.sum(diff_exp**2)
+            # Residual *= 1+np.sum((trace_model-trace_exp)**2)/np.sum(trace_exp**2)
+            # Residual *= 1+np.abs(1./np.corrcoef(diff_model,diff_exp)[0][1])
+            Residual *= 1+np.sum((diff_model-diff_exp)**2)/np.sum(diff_exp**2)
 
     except FileNotFoundError:
         print(sim.params_filename(index)+'.npz', 'not found')
