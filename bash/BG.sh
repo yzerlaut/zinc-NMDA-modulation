958dff36c41b64fe5ce3336a7b1af0041231a137
yzerlaut <yann.zerlaut@gmail.com>
2020-10-15 13:15:04 +0200

    forcing desktop version

--- bash/BG.sh
+++ bash/BG.sh
@@ -1,16 +1,8 @@
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 1 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 2 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 3 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 4 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 5 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 6 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 7 &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 8 
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 1 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 2 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 3 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 4 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 5 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 6 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 7 --chelated &
-python bg_modulation.py run --syn_location 1 --bg_levels 0 3 6 --NbgSEEDS 2 --seed 8 --chelated
+# python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 &
+# python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --chelated True &
+# python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --alphaZn 0.3 &
+# python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --chelated True --alphaZn 0.3 &
+python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --active&
+python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --active --chelated True &
+python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --active --alphaZn 0.3 &
+python bg_modulation.py full --bg_levels 0 2 4 6 8 --NbgSEEDS 2 --active --chelated True --alphaZn 0.3 &
